763d6ebbb80cc62ba8f862da45d5726f30239b7c
Hesham Hassan <74116013+heshamkeeni@users.noreply.github.com>
2022-10-05 15:11:20 +0300

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,75 +1,37 @@
+#include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
+ * main - adds positive numbers
+ * @argc: number of arguments passed to this program
+ * @argv: an array of pointers to strings
  *
- * Return: the int converted from the string
- */
-int _atoi(char *s)
-{
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-
-	if (f == 0)
-		return (0);
-
-	return (n);
-}
-
-/**
- * main - adds two positive number
- * @argc: number of arguments
- * @argv: array of arguents
- *
- * Return: 0 (Success), or 1 (Success)
+ * Return: Succesful execution 0, failed execution 1.
  */
 int main(int argc, char *argv[])
 {
-	int sum, num, i, j, k;
+	int i, sum = 0;
 
-	sum = 0;
-
+	if (argc == 1)
+	{
+		printf("%d\n", argc - 1);
+		return (0);
+	}
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		if ((*argv[i]) >= '0' && (*argv[i]) <= '9')
 		{
-			if (argv[i][j] > '9' || argv[i][j] < '0')
-			{
-				puts("Error");
-				return (1);
-			}
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
 		}
 	}
+	printf("%d\n", sum);
+	return (0);
+}
 
 	for (k = 1; k < argc; k++)
 	{
